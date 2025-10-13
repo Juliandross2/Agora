33d5b445987de9d297bbf3ec109a771ab654437a
Juliandross2 <julianalejandromunoz49@gmail.com>
2025-10-13 12:26:33 -0500

    HomeCreated

---
+++ frontend/agora_frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Agora</h1>
+      <p>Your one-stop solution for all things collaboration.</p>
+    </div>
+  );
+};
+
+export default Home;
