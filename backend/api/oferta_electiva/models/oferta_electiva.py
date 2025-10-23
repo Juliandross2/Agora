33b86bafbc6e2f1005c0f0d16d1db9cad6aa4036
Juliandross2 <julianalejandromunoz49@gmail.com>
2025-10-22 20:46:10 -0500

    Update oferta_electiva.py

--- backend/api/oferta_electiva/models/oferta_electiva.py
+++ backend/api/oferta_electiva/models/oferta_electiva.py
@@ -3,7 +3,7 @@ from django.db import models
 class OfertaElectiva(models.Model):
     oferta_electiva_id = models.AutoField(primary_key=True)
     electiva_id = models.ForeignKey('Electiva', on_delete=models.CASCADE)
-    materia_id = models.ForeignKey('Materia', on_delete=models.CASCADE)
+    #materia_id = models.ForeignKey('Materia', on_delete=models.CASCADE)
     periodo = models.IntegerField(null=False) # Ejemplo: 202401 para el primer semestre de 2024
     es_activa = models.BooleanField(default=True) # True si la oferta está activa, False si está inactiva
     
