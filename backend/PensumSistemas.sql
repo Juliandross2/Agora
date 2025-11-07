5917bb0cfcfe7e52e06d121efc417ef03f9c237f
Juliandross2 <julianalejandromunoz49@gmail.com>
2025-11-06 20:37:33 -0500

    Create PensumSistemas.sql

---
+++ backend/PensumSistemas.sql
@@ -0,0 +1,83 @@
+-- PRIMER SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Cálculo I', 4, 1, 1, 1, 2),
+('Lectura y escritura', 2, 1, 1, 1, 2),
+('Introducción a la Ingeniería de Sistemas', 1, 1, 1, 1, 2),
+('Introducción a la Informática', 3, 1, 1, 1, 2),
+('Laboratorio de Introducción a la Informática', 1, 1, 1, 1, 2);
+
+-- SEGUNDO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Cálculo II', 4, 1, 1, 2, 2),
+('Álgebra Lineal', 4, 1, 1, 2, 2),
+('Mecánica', 3, 1, 1, 2, 2),
+('Laboratorio de Mecánica', 1, 1, 1, 2, 2),
+('Programación Orientada a Objetos', 3, 1, 1, 2, 2),
+('Laboratorio de Programación Orientada a Objetos', 1, 1, 1, 2, 2);
+
+-- TERCER SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Cálculo III', 4, 1, 1, 3, 2),
+('Electromagnetismo', 3, 1, 1, 3, 2),
+('Laboratorio de Electromagnetismo', 1, 1, 1, 3, 2),
+('Estructura de Datos I', 3, 1, 1, 3, 2),
+('Laboratorio de Estructura de Datos I', 1, 1, 1, 3, 2);
+
+-- CUARTO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Ecuaciones Diferenciales Ordinarias', 4, 1, 1, 4, 2),
+('Vibraciones y Ondas', 3, 1, 1, 4, 2),
+('Estructura de Datos II', 3, 1, 1, 4, 2),
+('Laboratorio de Estructura de Datos II', 1, 1, 1, 4, 2),
+('Bases de Datos I', 3, 1, 1, 4, 2),
+('Laboratorio de Bases de Datos I', 1, 1, 1, 4, 2);
+
+-- QUINTO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Análisis Numérico', 4, 1, 1, 5, 2),
+('Teoría de la Computación', 3, 1, 1, 5, 2),
+('Bases de Datos II', 3, 1, 1, 5, 2),
+('Laboratorio de Bases de Datos II', 1, 1, 1, 5, 2),
+('Laboratorio de Ingeniería de Software', 1, 1, 1, 5, 2),
+('Ingeniería de Software I', 3, 1, 1, 5, 2),
+('Arquitectura Computacional', 4, 1, 1, 5, 2);
+
+-- SEXTO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Estadística y Probabilidad', 4, 1, 1, 6, 2),
+('Estructuras de Lenguajes', 3, 1, 1, 6, 2),
+('Laboratorio de Estructuras de Lenguajes', 1, 1, 1, 6, 2),
+('Ingeniería de Software II', 3, 1, 1, 6, 2),
+('Laboratorio de Ingeniería de Software II', 1, 1, 1, 6, 2),
+('Sistemas Operativos', 3, 1, 1, 6, 2),
+('Laboratorio de Sistemas Operativos', 1, 1, 1, 6, 2);
+
+-- SÉPTIMO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Metodología de la Investigación', 3, 1, 1, 7, 2),
+('Inteligencias Artificial', 3, 1, 1, 7, 2),
+('Teoría Dinámica de Sistemas', 3, 1, 1, 7, 2),
+('Sistemas Distribuidos', 3, 1, 1, 7, 2),
+('Laboratorio de Sistemas Distribuidos', 1, 1, 1, 7, 2),
+('Ingeniería de Software III', 3, 1, 1, 7, 2),
+('Laboratorio de Ingeniería de Software III', 1, 1, 1, 7, 2);
+
+-- OCTAVO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Redes', 4, 1, 1, 8, 2),
+('Investigación de Operaciones', 4, 1, 1, 8, 2),
+('Proyecto I', 3, 1, 1, 8, 2),
+('Calidad de Software', 3, 1, 1, 8, 2);
+
+-- NOVENO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Gestión empresarial', 3, 1, 1, 9, 2),
+('Gestión de Proyectos Informáticos', 3, 1, 1, 9, 2),
+('Proyecto II', 3, 1, 1, 9, 2),
+('Fundamentos de Economía', 3, 1, 1, 9, 2),
+('Legislación Laboral', 1, 1, 1, 9, 2);
+
+-- DÉCIMO SEMESTRE
+INSERT INTO api_materia (nombre_materia, creditos, es_obligatoria, es_activa, semestre, pensum_id_id) VALUES
+('Trabajo de grado', 14, 1, 1, 10, 2),
+('Ética', 2, 1, 1, 10, 2);
